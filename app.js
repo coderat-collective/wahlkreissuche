323da29ca775fda12b59cd4a31e16ca67e61019b
tlmn <mail@tlmn.io>
2021-10-07 13:54:51 +0200

    remove unneeded imports

--- app.js
+++ app.js
@@ -2,16 +2,11 @@ var createError = require('http-errors')
 var express = require('express')
 var path = require('path')
 var cookieParser = require('cookie-parser')
-var logger = require('morgan')
 var cors = require('cors')
 
 var indexRouter = require('./routes/index')
 
 var app = express()
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'jade')
 
 app.use(
   cors({
@@ -19,7 +14,6 @@ app.use(
   })
 )
 
-app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
@@ -27,12 +21,10 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404))
 })
 
-// error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message
